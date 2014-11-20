aa4f2ddeb86070950504f6a0d97125894281f1e9
Mohsen Vakilian <reprogrammer@gmail.com>
2014-11-20 13:51:43 -0800

    Refactored IBS to reuse GenomicsDatasetOptions.

--- src/main/java/com/google/cloud/genomics/dataflow/pipelines/IdentityByState.java
+++ src/main/java/com/google/cloud/genomics/dataflow/pipelines/IdentityByState.java
@@ -17,139 +17,53 @@ package com.google.cloud.genomics.dataflow.pipelines;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 import java.util.List;
-import java.util.logging.Logger;
 
-import com.google.api.services.genomics.model.ReferenceBound;
 import com.google.api.services.genomics.model.SearchVariantsRequest;
-import com.google.api.services.genomics.model.VariantSet;
 import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.io.TextIO;
-import com.google.cloud.dataflow.sdk.runners.Description;
 import com.google.cloud.dataflow.sdk.transforms.Combine;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
 import com.google.cloud.dataflow.sdk.values.KV;
 import com.google.cloud.dataflow.utils.OptionsParser;
-import com.google.cloud.dataflow.utils.RequiredOption;
 import com.google.cloud.genomics.dataflow.functions.FormatIBSData;
 import com.google.cloud.genomics.dataflow.functions.IBSCalculator;
 import com.google.cloud.genomics.dataflow.functions.SharedAllelesCounter;
 import com.google.cloud.genomics.dataflow.readers.VariantReader;
 import com.google.cloud.genomics.dataflow.utils.DataflowWorkarounds;
 import com.google.cloud.genomics.dataflow.utils.GenomicsAuth;
-import com.google.cloud.genomics.dataflow.utils.GenomicsOptions;
-import com.google.common.collect.Lists;
+import com.google.cloud.genomics.dataflow.utils.GenomicsDatasetOptions;
 
 /**
- * An pipeline that computes Identity by State (IBS) for variants in a dataset.
+ * A pipeline that computes Identity by State (IBS) for each pair of individuals in a dataset.
  *
- * To access the Google Genomics API, you need to either provide a valid API key or a
- * client_secrets.json file.
- *
- * Follow the instructions at http://developers.google.com/genomics to generate a
- * client_secrets.json file, and move it into the directory that you run the pipeline from.
- *
- * Alternatively, specify the API key with this flag: --apiKey=<API_KEY>
- *
- *
- * To execute this pipeline locally, specify general pipeline configuration: --project=<PROJECT ID>
- * and example configuration: --output=[<LOCAL FILE> | gs://<OUTPUT PATH>]
- *
- * To execute this pipeline using the Dataflow service, specify pipeline configuration:
- * --project=<PROJECT ID> --stagingLocation=gs://<STAGING DIRECTORY>
- * --runner=BlockingDataflowPipelineRunner and example configuration: --output=gs://<OUTPUT PATH>
- *
- * The input datasetId defaults to 10473108253681171589 (1000 genomes) and can be overridden with
- * --datasetId.
+ * See the README for running instructions.
  */
 public class IdentityByState {
-  private static final Logger LOG = Logger.getLogger(IdentityByState.class.getName());
   private static final String VARIANT_FIELDS = "nextPageToken,variants(id,calls(genotype,callSetName))";
 
-  private static List<SearchVariantsRequest> getVariantRequests(GenomicsAuth auth, Options options)
-      throws IOException, GeneralSecurityException {
-    if (options.allContigs) {
-      List<SearchVariantsRequest> requests = Lists.newArrayList();
-
-      VariantSet variantSet = auth.getService().variantsets().get(options.datasetId).execute();
-      for (ReferenceBound bound : variantSet.getReferenceBounds()) {
-        String contig = bound.getReferenceName().toLowerCase();
-        if (contig.contains("x") || contig.contains("y")) {
-          // X and Y skew PCA results
-          continue;
-        }
-
-        requests.addAll(getShardedRequests(variantSet.getId(),
-            bound.getReferenceName(), 0, bound.getUpperBound()));
-      }
-      return requests;
-
-    } else {
-      // If not running all contigs, we default to BRCA1
-      // TODO: Different data sets refer to the same chromosome differently. Illumina
-      // refers to it as "chr17" while 1000 Genomes refers to it as "17". We need to take this
-      // difference into account when referring to BRCA1.
-      return getShardedRequests(options.datasetId, "chr17", 41196312, 41277500);
-    }
-  }
-
-  private static List<SearchVariantsRequest> getShardedRequests(String variantSetId, String contig,
-      long start, long end) {
-
-    long basesPerShard = 1000000; // 1 million
-
-    double shards = Math.ceil((end - start) / (double) basesPerShard);
-    List<SearchVariantsRequest> requests = Lists.newArrayList();
-    for (int i = 0; i < shards; i++) {
-      long shardStart = start + (i * basesPerShard);
-      long shardEnd = Math.min(end, shardStart + basesPerShard);
-
-      LOG.info("Adding request with " + contig + " " + shardStart + " to " + shardEnd);
-      requests.add(new SearchVariantsRequest()
-          .setVariantSetIds(Collections.singletonList(variantSetId))
-          .setReferenceName(contig)
-          .setStart(shardStart)
-          .setEnd(shardEnd));
-
-    }
-    return requests;
-  }
-
-  private static class Options extends GenomicsOptions {
-    @Description("Path of the file to write to")
-    @RequiredOption
-    public String output;
-
-    @Description("The ID of the Google Genomics dataset this pipeline is working with. " +
-        "Defaults to 1000 Genomes.")
-    public String datasetId = "10473108253681171589";
-
-    @Description("By default, PCA will be run on BRCA1, pass this flag to run on all " +
-        "non X and Y contigs present in the dataset")
-    public boolean allContigs = false;
-  }
-
   public static void main(String[] args) throws IOException, GeneralSecurityException {
-    Options options = OptionsParser.parse(args, Options.class,
-        IdentityByState.class.getSimpleName());
+    GenomicsDatasetOptions options =
+        OptionsParser.parse(args, GenomicsDatasetOptions.class,
+            IdentityByState.class.getSimpleName());
     options.validateOptions();
 
     GenomicsAuth auth = options.getGenomicsAuth();
 
-    List<SearchVariantsRequest> requests = getVariantRequests(auth, options);
+    List<SearchVariantsRequest> requests = options.getVariantRequests(auth);
 
     Pipeline p = Pipeline.create(options);
     DataflowWorkarounds.registerGenomicsCoders(p);
     DataflowWorkarounds
         .getPCollection(requests, p, options.numWorkers)
-        .apply(ParDo.named("VariantReader").of(new VariantReader(auth, VARIANT_FIELDS)))
         .apply(
-            ParDo.named("SharedAllelesCallsOfVariantCounter").of(
-                new SharedAllelesCounter()))
+            ParDo.named(VariantReader.class.getSimpleName()).of(
+                new VariantReader(auth, VARIANT_FIELDS)))
+        .apply(
+            ParDo.named(SharedAllelesCounter.class.getSimpleName()).of(new SharedAllelesCounter()))
         .apply(Combine.<KV<String, String>, KV<Double, Integer>>perKey(new IBSCalculator()))
-        .apply(ParDo.named("FormatIBSData").of(new FormatIBSData()))
-        .apply(TextIO.Write.named("WriteIBS").to(options.output));
+        .apply(ParDo.named(FormatIBSData.class.getSimpleName()).of(new FormatIBSData()))
+        .apply(TextIO.Write.named("WriteIBSData").to(options.output));
 
     p.run();
   }
--- src/main/java/com/google/cloud/genomics/dataflow/pipelines/VariantSimilarity.java
+++ src/main/java/com/google/cloud/genomics/dataflow/pipelines/VariantSimilarity.java
@@ -31,7 +31,7 @@ import java.security.GeneralSecurityException;
 import java.util.List;
 
 /**
- * An pipeline that generates similarity data for variants in a dataset.
+ * A pipeline that generates similarity data for variants in a dataset.
  * See the README for running instructions.
  */
 public class VariantSimilarity {
