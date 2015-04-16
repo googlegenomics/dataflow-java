f4c57c5f1517002533cc1de85ebec936f1a0d535
JP Martin <jpmartin@google.com>
2015-04-16 16:48:54 -0700

    m

--- src/main/java/com/google/cloud/genomics/dataflow/utils/GCSFilename.java
+++ src/main/java/com/google/cloud/genomics/dataflow/utils/GCSFilename.java
@@ -1,6 +1,6 @@
 package com.google.cloud.genomics.dataflow.utils;
 
-import com.google.api.client.repackaged.com.google.common.base.Preconditions;
+import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
 import java.io.IOException;
