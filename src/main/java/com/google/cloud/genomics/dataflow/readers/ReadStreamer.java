91a8140eac90a21b16f47a60f1fde084ca54af13
Carey Macdonald <careyjmac@google.com>
2015-06-17 11:09:15 -0700

    Adding classes to assist with streaming from gRPC

---
+++ src/main/java/com/google/cloud/genomics/dataflow/readers/ReadStreamer.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.genomics.dataflow.readers;
+
+import com.google.api.services.genomics.model.ReadGroupSet;
+import com.google.api.services.genomics.model.SearchReadGroupSetsRequest;
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
+import com.google.cloud.dataflow.sdk.transforms.PTransform;
+import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.values.PCollection;
+import com.google.cloud.genomics.grpc.Channels;
+import com.google.cloud.genomics.utils.Contig;
+import com.google.cloud.genomics.utils.GenomicsFactory;
+import com.google.cloud.genomics.utils.Paginator;
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Lists;
+import com.google.genomics.v1.Read;
+import com.google.genomics.v1.StreamReadsRequest;
+import com.google.genomics.v1.StreamReadsResponse;
+import com.google.genomics.v1.StreamingReadServiceGrpc;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Class with tools for streaming reads using gRPC within dataflow pipelines.
+ */
+public class ReadStreamer {
+
+  /**
+   * Gets ReadGroupSetIds from a given datasetId using the Genomics API.
+   */
+  public static List<String> getReadGroupSetIds(String datasetId, GenomicsFactory.OfflineAuth auth)
+      throws IOException, GeneralSecurityException {
+    List<String> output = Lists.newArrayList();
+    Iterable<ReadGroupSet> rgs = Paginator.ReadGroupSets.create(
+        auth.getGenomics(auth.getDefaultFactory()))
+        .search(new SearchReadGroupSetsRequest().setDatasetIds(Lists.newArrayList(datasetId)),
+            "readGroupSets/id,nextPageToken");
+    for (ReadGroupSet r : rgs) {
+      output.add(r.getId());
+    }
+    if (output.isEmpty()) {
+      throw new IOException("Dataset " + datasetId + " does not contain any ReadGroupSets");
+    }
+    return output;
+  }
+
+  /**
+   * Constructs a StreamReadsRequest for a readGroupSetId, assuming that the user wants all
+   * to include all references.
+   */
+  public static StreamReadsRequest getReadRequests(String readGroupSetId) {
+    return StreamReadsRequest.newBuilder()
+        .setReadGroupSetId(readGroupSetId)
+        .build();
+  }
+
+  /**
+   * Constructs a StreamReadsRequest for a readGroupSetId and a set of user provided references.
+   */
+  public static List<StreamReadsRequest> getReadRequests(final String readGroupSetId,
+      String references) {
+    final Iterable<Contig> contigs = Contig.parseContigsFromCommandLine(references);
+    return FluentIterable.from(contigs)
+        .transformAndConcat(new Function<Contig, Iterable<Contig>>() {
+            @Override
+            public Iterable<Contig> apply(Contig contig) {
+              return contig.getShards(100000000L);
+            }
+          })
+        .transform(new Function<Contig, StreamReadsRequest>() {
+          @Override
+          public StreamReadsRequest apply(Contig shard) {
+            return StreamReadsRequest.newBuilder()
+            .setReadGroupSetId(readGroupSetId)
+            .setStart(shard.start)
+            .setEnd(shard.end)
+            .setReferenceName(shard.referenceName)
+            .build();
+          }
+        }).toList();
+
+  }
+
+  /**
+   * PTransform for streaming reads via gRPC.
+   */
+  public static class StreamReads extends
+      PTransform<PCollection<StreamReadsRequest>, PCollection<Read>> {
+
+    @Override
+    public PCollection<Read> apply(PCollection<StreamReadsRequest> input) {
+      return input.apply(ParDo.of(new RetrieveReads()))
+          .apply(ParDo.of(new ConvergeReadsList()));
+    }
+  }
+
+  private static class RetrieveReads extends DoFn<StreamReadsRequest, List<Read>> {
+
+    private transient StreamingReadServiceGrpc.StreamingReadServiceBlockingStub readStub;
+
+    @Override
+    public void startBundle(Context c) throws IOException {
+      this.readStub = StreamingReadServiceGrpc.newBlockingStub(Channels.fromDefaultCreds());
+    }
+
+    @Override
+    public void processElement(ProcessContext c) {
+      Iterator<StreamReadsResponse> iter = readStub.streamReads(c.element());
+      while (iter.hasNext()) {
+        StreamReadsResponse readResponse = iter.next();
+        c.output(readResponse.getAlignmentsList());
+      }
+    }
+  }
+
+  /**
+   * This step exists to emit the individual reads in a parallel step to the StreamReads step in
+   * order to increase throughput.
+   */
+  private static class ConvergeReadsList extends DoFn<List<Read>, Read> {
+
+    @Override
+    public void processElement(ProcessContext c) {
+      for (Read r : c.element()) {
+        c.output(r);
+      }
+    }
+  }
+}
