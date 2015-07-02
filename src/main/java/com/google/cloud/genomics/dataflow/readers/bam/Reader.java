bdf1875bba704b290c76b3b58136549813e55171
Ilia Tulchinsky <iliat@google.com>
2015-07-02 09:54:53 -0400

    Adding option to read unmapped reads as expected by GATK/Picard

--- src/main/java/com/google/cloud/genomics/dataflow/readers/bam/Reader.java
+++ src/main/java/com/google/cloud/genomics/dataflow/readers/bam/Reader.java
@@ -44,22 +44,48 @@ public class Reader {
   BAMShard shard;
   DoFn<BAMShard, Read>.ProcessContext c;
   Stopwatch timer;
-  ValidationStringency stringency;
+  ReaderOptions options;
   
   SAMRecordIterator iterator;
-  boolean procesingUnmapped;
+  enum Filter {
+    UNMAPPED_ONLY,
+    MAPPED_AND_UNMAPPED,
+    MAPPED_ONLY
+  }
+  
+  Filter filter;
   
   int recordsBeforeStart = 0;
   int recordsAfterEnd = 0;
   int mismatchedSequence = 0;
   int recordsProcessed = 0;
   
-  public Reader(Objects storageClient, ValidationStringency stringency, BAMShard shard, DoFn<BAMShard, Read>.ProcessContext c) {
+  public Reader(Objects storageClient, ReaderOptions options, BAMShard shard, DoFn<BAMShard, Read>.ProcessContext c) {
     super();
     this.storageClient = storageClient;
     this.shard = shard;
     this.c = c;
-    this.stringency = stringency;
+    this.options = options;
+    filter = setupFilter(options, shard.contig.referenceName);
+  }
+  
+  public static Filter setupFilter(ReaderOptions options, String referenceName) {
+    /* 
+     * The common way of asking for unmapped reads is by specifying asterisk
+     * as the sequence name.
+     * From SAM format, section 1.4. paragraph 3:
+     * "RNAME: Reference sequence NAME of the alignment. 
+     * If @SQ header lines are present, RNAME (if not ‘*’) must be present in 
+     * one of the SQ-SN tag. 
+     * An unmapped segment without coordinate has a ‘*’ at this field."
+     * https://samtools.github.io/hts-specs/SAMv1.pdf
+     */
+    if (referenceName.equals("*")) {
+      return Filter.UNMAPPED_ONLY;
+    } else if (options.getIncludeUnmappedReads()) {
+      return Filter.MAPPED_AND_UNMAPPED;
+    }
+    return Filter.MAPPED_ONLY;
   }
   
   public void process() throws IOException {
@@ -75,11 +101,11 @@ public class Reader {
 
   void openFile() throws IOException {
     LOG.info("Processing shard " + shard);
-    final SamReader reader = BAMIO.openBAM(storageClient, shard. file, stringency);
+    final SamReader reader = BAMIO.openBAM(storageClient, shard.file, 
+        options.getStringency());
     iterator = null;
-    procesingUnmapped = shard.contig.referenceName.equals("*");
     if (reader.hasIndex() && reader.indexing() != null) {
-      if (procesingUnmapped) {
+      if (filter == Filter.UNMAPPED_ONLY) {
         LOG.info("Processing unmapped");
         iterator = reader.queryUnmapped();
       } else if (shard.span != null) {
@@ -97,16 +123,43 @@ public class Reader {
     }
   }
 
-  boolean isWrongSequence(SAMRecord record) {
-    return (procesingUnmapped && !record.getReadUnmappedFlag()) ||
-        (!procesingUnmapped && (record.getReadUnmappedFlag() || 
-            (shard.contig.referenceName != null && 
-            !shard.contig.referenceName.isEmpty() &&
-            !shard.contig.referenceName.equals(record.getReferenceName()))));
+  /**
+   * Checks if the record matches our filter.
+   */
+  static boolean passesFilter(SAMRecord record, Filter filter, 
+      String referenceName) {
+    // If we are looking for only mapped or only unmapped reads then we will use
+    // the UnmappedFlag to decide if this read should be rejected.
+    if (filter == Filter.UNMAPPED_ONLY && !record.getReadUnmappedFlag()) {
+      return false;
+    }
+    
+    if (filter == Filter.MAPPED_ONLY && record.getReadUnmappedFlag()) {
+      return false;
+    }
+    
+    // If we are looking for mapped reads, then we check the reference name
+    // of the read matches the one we are looking for.
+    final boolean referenceNameMismatch = referenceName != null && 
+        !referenceName.isEmpty() &&
+        !referenceName.equals(record.getReferenceName());
+    
+    // Note that unmapped mate pair of mapped read will have a reference
+    // name set to the reference of its mapped mate.
+    if ((filter == Filter.MAPPED_ONLY || filter == Filter.MAPPED_AND_UNMAPPED)
+        && referenceNameMismatch) {
+      return false;
+    }
+    
+    return true;
+  }
+  
+  boolean passesFilter(SAMRecord record) {
+    return passesFilter(record, filter, shard.contig.referenceName);
   }
   
   void processRecord(SAMRecord record) {
-    if (isWrongSequence(record)) {
+    if (!passesFilter(record)) {
       mismatchedSequence++;
       return;
     }
@@ -138,9 +191,11 @@ public class Reader {
    * This makes it easier to discover errors such as reads that are somehow
    * skipped by a sharded approach.
    */
-  public static Iterable<Read> readSequentiallyForTesting(Objects storageClient, String storagePath, Contig contig, ValidationStringency stringency) throws IOException {
+  public static Iterable<Read> readSequentiallyForTesting(Objects storageClient,
+      String storagePath, Contig contig, 
+      ReaderOptions options) throws IOException {
     Stopwatch timer = Stopwatch.createStarted();
-    SamReader samReader = BAMIO.openBAM(storageClient, storagePath, stringency);
+    SamReader samReader = BAMIO.openBAM(storageClient, storagePath, options.getStringency());
     SAMRecordIterator iterator =  samReader.queryOverlapping(contig.referenceName, 
         (int) contig.start + 1,
         (int) contig.end + 1);
@@ -150,12 +205,12 @@ public class Reader {
     int recordsAfterEnd = 0;
     int mismatchedSequence = 0;
     int recordsProcessed = 0;
+    Filter filter = setupFilter(options, contig.referenceName);
     while (iterator.hasNext()) {
       SAMRecord record = iterator.next();
-      final boolean wrongSequence = !contig.referenceName.equals(record.getReferenceName())
-          || (!contig.referenceName.equals("*") && record.getReadUnmappedFlag());
+      final boolean passesFilter = passesFilter(record, filter, contig.referenceName);
       
-      if (wrongSequence) {
+      if (!passesFilter) {
         mismatchedSequence++;
         continue;
       }
