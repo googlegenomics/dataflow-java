1b92fb6754c705a3d4b602cf5434179de940855a
Nicole Deflaux <deflaux@google.com>
2016-04-13 17:56:18 -0700

    Fix package paths.

--- src/test/java/com/google/cloud/genomics/dataflow/functions/VariantFunctionsTest.java
+++ src/test/java/com/google/cloud/genomics/dataflow/functions/VariantFunctionsTest.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.cloud.genomics.dataflow.functions.verifybamid;
+package com.google.cloud.genomics.dataflow.functions;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
