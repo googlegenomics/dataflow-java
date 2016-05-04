04f0fcc73e0609d04a7cdfd2d6b7b5b5f9e152f3
Nicole Deflaux <deflaux@google.com>
2016-05-04 12:20:32 -0700

    Add IdentifyPrivateVariants and DeleteVariants pipelines.

---
+++ src/main/java/com/google/cloud/genomics/dataflow/pipelines/DeleteVariants.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.genomics.dataflow.pipelines;
+
+import com.google.api.client.util.BackOff;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.genomics.Genomics;
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.io.TextIO;
+import com.google.cloud.dataflow.sdk.options.Description;
+import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
+import com.google.cloud.dataflow.sdk.options.Validation.Required;
+import com.google.cloud.dataflow.sdk.transforms.Aggregator;
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
+import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.transforms.Sum;
+import com.google.cloud.genomics.dataflow.utils.GCSOutputOptions;
+import com.google.cloud.genomics.dataflow.utils.GenomicsOptions;
+import com.google.cloud.genomics.utils.GenomicsFactory;
+import com.google.cloud.genomics.utils.OfflineAuth;
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+/**
+ * Given a file of variant ids, delete them from a variant set in parallel.
+ *
+ * These variant ids may from from the result of a BigQuery query materialized to Cloud Storage
+ * as CSV or as the result of a Dataflow pipeline such as IdentifyPrivateVariants.
+ */
+public class DeleteVariants {
+
+  public static interface Options extends GCSOutputOptions {
+    @Description("The filepath to a comma-separated or tab-separated file of variant ids. "
+        + "The variant id will be retrieved from the first "
+        + "column.  Any other columns will be ignored.")
+    @Required
+    String getInput();
+    void setInput(String filepath);
+
+    public static class Methods {
+      public static void validateOptions(Options options) {
+        GCSOutputOptions.Methods.validateOptions(options);
+      }
+    }
+  }
+
+  private static final Logger LOG = LoggerFactory.getLogger(DeleteVariants.class);
+
+  public static final class DeleteVariantFn extends DoFn<String, Integer> {
+    private final OfflineAuth auth;
+    private final Aggregator<Long, Long> notFoundVariantCount =
+        createAggregator("Number of variants not found (previously deleted)", new Sum.SumLongFn());
+    private final Aggregator<Long, Long> deletedVariantCount =
+        createAggregator("Number of variants deleted", new Sum.SumLongFn());
+    private Genomics genomics;
+
+    public DeleteVariantFn(OfflineAuth auth) {
+      super();
+      this.auth = auth;
+    }
+
+    @Override
+    public void startBundle(Context context) throws IOException, GeneralSecurityException {
+      genomics = GenomicsFactory.builder().build().fromOfflineAuth(auth);
+    }
+
+    @Override
+    public void processElement(DoFn<String, Integer>.ProcessContext context) throws Exception {
+      String variantId = context.element();
+      // Call the deletion operation via exponential backoff so that "Rate Limit Exceeded"
+      // quota issues do not cause the pipeline to fail.
+      ExponentialBackOff backoff = new ExponentialBackOff.Builder().build();
+      while (true) {
+        try {
+          genomics.variants().delete(variantId).execute();
+          deletedVariantCount.addValue(1L);
+          context.output(1);
+          return;
+        } catch (Exception e) {
+          if (e.getMessage().startsWith("403 Forbidden")) {
+            long backOffMillis = backoff.nextBackOffMillis();
+            if (backOffMillis == BackOff.STOP) {
+              throw e;
+            }
+            Thread.sleep(backOffMillis);
+          } else {
+            throw e;
+          }
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) throws IOException, GeneralSecurityException {
+    // Register the options so that they show up via --help
+    PipelineOptionsFactory.register(Options.class);
+    Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+    // Option validation is not yet automatic, we make an explicit call here.
+    Options.Methods.validateOptions(options);
+
+    OfflineAuth auth = GenomicsOptions.Methods.getGenomicsAuth(options);
+
+    GenomicsOptions.Methods.requestConfirmation("*** The pipeline will delete variants whose "
+        + "ids are listed in: " + options.getInput() + ". ***");
+
+    Pipeline p = Pipeline.create(options);
+
+    p.apply(TextIO.Read.named("ReadLines").from(options.getInput()))
+        .apply(ParDo.named("ParseVariantIds").of(new DoFn<String, String>() {
+          @Override
+          public void processElement(ProcessContext c) {
+            String record = c.element();
+
+            // The variant id will be retrieved from the first column.  Any other columns
+            // will be ignored.
+            Iterable<String> fields =
+                Splitter.on(
+                    CharMatcher.BREAKING_WHITESPACE.or(CharMatcher.is(',')))
+                    .omitEmptyStrings()
+                    .trimResults()
+                    .split(record);
+            java.util.Iterator<String> iter = fields.iterator();
+            if (iter.hasNext()) {
+              c.output(iter.next());
+            }
+          }
+        }))
+        .apply(ParDo.of(new DeleteVariantFn(auth)))
+        .apply(Sum.integersGlobally())
+        .apply(ParDo.named("FormatResults").of(new DoFn<Integer, String>() {
+          @Override
+          public void processElement(ProcessContext c) {
+            c.output("Deleted Variant Count: " + c.element());
+          }
+        }))
+        .apply(TextIO.Write.named("Write Count").to(options.getOutput()));
+
+    p.run();
+  }
+}
---
+++ src/main/java/com/google/cloud/genomics/dataflow/pipelines/IdentifyPrivateVariants.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.genomics.dataflow.pipelines;
+
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.io.TextIO;
+import com.google.cloud.dataflow.sdk.options.Default;
+import com.google.cloud.dataflow.sdk.options.Description;
+import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
+import com.google.cloud.dataflow.sdk.options.Validation.Required;
+import com.google.cloud.dataflow.sdk.repackaged.com.google.common.collect.ImmutableSet;
+import com.google.cloud.dataflow.sdk.transforms.Create;
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
+import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.values.PCollection;
+import com.google.cloud.genomics.dataflow.readers.VariantStreamer;
+import com.google.cloud.genomics.dataflow.utils.GCSOutputOptions;
+import com.google.cloud.genomics.dataflow.utils.GenomicsOptions;
+import com.google.cloud.genomics.dataflow.utils.ShardOptions;
+import com.google.cloud.genomics.utils.OfflineAuth;
+import com.google.cloud.genomics.utils.ShardBoundary;
+import com.google.cloud.genomics.utils.ShardUtils;
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.io.Files;
+import com.google.genomics.v1.StreamVariantsRequest;
+import com.google.genomics.v1.Variant;
+import com.google.genomics.v1.VariantCall;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+/**
+ * Given a list of callset ids, identify all variants private to those individuals.
+ *
+ * This pipeline might be used in combination with the DeleteVariants pipeline to, for example,
+ * remove all variants for a particular family from the variant set.
+ */
+public class IdentifyPrivateVariants {
+
+  public static interface Options extends
+  // Options for calculating over regions, chromosomes, or whole genomes.
+  ShardOptions,
+  // Options for the output destination.
+  GCSOutputOptions {
+
+    @Description("The ID of the Google Genomics variant set from which this pipeline "
+        + "will identify private variants.")
+    @Required
+    String getVariantSetId();
+    void setVariantSetId(String variantSetId);
+
+    @Description("A local file path to a list of newline-separated callset IDs. "
+        + "Any variants private to those callset IDs will be deleted.")
+    @Required
+    String getCallSetIdsFilepath();
+    void setCallSetIdsFilepath(String filepath);
+
+    @Description("Whether variants with no callsets should also be identified.  Defaults to false.")
+    @Default.Boolean(false)
+    boolean getIdentifyVariantsWithoutCalls();
+    void setIdentifyVariantsWithoutCalls(boolean identifyVariantsWithoutCalls);
+
+    public static class Methods {
+      public static void validateOptions(Options options) {
+        GCSOutputOptions.Methods.validateOptions(options);
+      }
+    }
+  }
+
+  private static final Logger LOG = LoggerFactory.getLogger(IdentifyPrivateVariants.class);
+  private static final String VARIANT_FIELDS = "variants(id,reference_name,start,end,reference_bases,alternate_bases,calls(callSetId))";
+
+  /**
+   * Pipeline function implementing a filter only returning variants private to one or more callset
+   * IDs and optionally those with no callsetIds.
+   */
+  public static final class PrivateVariantsFilterFn extends DoFn<Variant, Variant> {
+
+    private final ImmutableSet<String> callSetIds;
+    private boolean retainVariantsWithNoCalls;
+
+    /**
+     * @param callSetIds
+     */
+    public PrivateVariantsFilterFn(ImmutableSet<String> callSetIds,
+        boolean retainVariantsWithNoCalls) {
+      super();
+      this.callSetIds = callSetIds;
+      this.retainVariantsWithNoCalls = retainVariantsWithNoCalls;
+    }
+
+    @Override
+    public void processElement(ProcessContext context) {
+      Variant variant = context.element();
+      List<VariantCall> calls = variant.getCallsList();
+
+      for (VariantCall call : calls) {
+        if (!callSetIds.contains(call.getCallSetId())) {
+          // We found a callset ID not in our set. This variant is not private
+          // to our set of callset IDs. Skip it.
+          return;
+        }
+      }
+
+      if (!retainVariantsWithNoCalls && calls.isEmpty()) {
+        // This is a variant with no calls.  Skip it.
+        return;
+      }
+
+      context.output(variant);
+    }
+  }
+
+  public static void main(String[] args) throws IOException, GeneralSecurityException {
+    // Register the options so that they show up via --help
+    PipelineOptionsFactory.register(Options.class);
+    Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+    // Option validation is not yet automatic, we make an explicit call here.
+    Options.Methods.validateOptions(options);
+
+    OfflineAuth auth = GenomicsOptions.Methods.getGenomicsAuth(options);
+
+    // Grab and parse the list of callset IDs.
+    String fileContents =
+        Files.toString(new File(options.getCallSetIdsFilepath()), Charset.defaultCharset());
+    ImmutableSet<String> callSetIds =
+        ImmutableSet
+            .<String>builder()
+            .addAll(
+                Splitter.on(CharMatcher.BREAKING_WHITESPACE).omitEmptyStrings().trimResults()
+                    .split(fileContents)).build();
+    LOG.info("The pipeline will identify and write to Cloud Storage variants "
+        + "private to " + callSetIds.size() + " genomes with callSetIds: " + callSetIds);
+    if (options.getIdentifyVariantsWithoutCalls()) {
+      LOG.info("* The pipeline will also identify variants with no callsets. *");
+    }
+
+    List<StreamVariantsRequest> shardRequests =
+        options.isAllReferences() ? ShardUtils.getVariantRequests(options.getVariantSetId(),
+            ShardUtils.SexChromosomeFilter.INCLUDE_XY, options.getBasesPerShard(), auth)
+            : ShardUtils.getVariantRequests(options.getVariantSetId(), options.getReferences(),
+                options.getBasesPerShard());
+
+    Pipeline p = Pipeline.create(options);
+    PCollection<Variant> variants = p.begin()
+        .apply(Create.of(shardRequests))
+        .apply(new VariantStreamer(auth, ShardBoundary.Requirement.STRICT, VARIANT_FIELDS))
+        .apply(ParDo.of(new PrivateVariantsFilterFn(callSetIds,
+            options.getIdentifyVariantsWithoutCalls())));
+
+    variants.apply(ParDo.named("FormatResults").of(new DoFn<Variant, String>() {
+      @Override
+      public void processElement(ProcessContext c) {
+        Variant v = c.element();
+        c.output(Joiner.on("\t").join(v.getId(),
+            v.getReferenceName(),
+            v.getStart(),
+            v.getEnd(),
+            v.getReferenceBases(),
+            Joiner.on(",").join(v.getAlternateBasesList())
+            ));
+      }
+    }))
+    .apply(TextIO.Write.to(options.getOutput()));
+
+    p.run();
+  }
+}
